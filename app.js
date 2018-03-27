e5ae99147363cb675e4235483078e66add82ad07
Tom Klinovsky <tom@kiwinet.co.il>
2018-03-27 15:23:25 +0300

    implemented login before fetch

--- app.js
+++ app.js
@@ -1,12 +1,43 @@
 const http = require('http');
+const cheerio = require('cheerio');
+const request = require('request');
+
 const parseWifiClients = require('./project_modules/wifi_client_parser');
 
-function fetchConnectedWifiClients(router_ip, path) {
+function loginToRouter({router_ip, username, password, path}) {
+  return new Promise((resolve, reject) => {
+    let post_data = {
+      loginUsername: username,
+      loginPassword: password,
+      rememberMe: "rememberMe"
+    }
+
+    path = path || '/goform/login';
+
+    request.post(
+      'http://' + router_ip + path,
+      { form: post_data },
+      function(err, res, body) {
+        if (res.headers['location'] === '/login.asp') {
+          reject(new Error("login failed"));
+        } else if (res.headers['location'] === '/RgSwInfo.asp') {
+          resolve();
+        } else {
+          reject(new Error("unknown login error"));
+        }
+      }
+    );
+  });
+}
+
+async function fetchConnectedWifiClients({router_ip, username, password, path}) {
+  await loginToRouter({router_ip, username, password});
+
   return new Promise((resolve, reject) => {
     if (typeof router_ip !== 'string') {
       reject(new Error('no valid router_ip provided'))
     }
-
+    
     http.get({
       host: router_ip,
       port: 80,
@@ -16,11 +47,20 @@ function fetchConnectedWifiClients(router_ip, path) {
       let data = '';
       res.on('data', chunk => data += chunk);
       res.on('end', () => {
-        resolve(parseWifiClients(data));
+        const $ = cheerio.load(data);
+        if ($('h1').html().includes("Login")) {
+          console.log("not logged in")
+          resolve([]);
+        } else {
+          resolve(parseWifiClients(data));
+        }
       });
     });
   })
 }
 
-fetchConnectedWifiClients('192.168.1.1')
-  .then(clients => console.log(clients));
+fetchConnectedWifiClients({
+  router_ip: '192.168.1.1',
+  username: 'admin',
+  password: 'admin'
+}).then(clients => console.log(clients));
