0069ff6e39b26a67ee0df593d505f131bc9ad6e3
Tom Klinovsky <tom@kiwinet.co.il>
2018-03-27 13:46:37 +0300

    enclosed wifi clients fetching process in a promise

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 const http = require('http');
 const parseWifiClients = require('./project_modules/wifi_client_parser');
 
-http.get({
-  host: '192.168.1.1',
-  port: 80,
-  path: '/wlanAccess.asp',
-  agent: false,
-  }, (res) => {
-  let data = '';
-  res.on('data', chunk => data += chunk);
-  res.on('end', () => { console.log(parseWifiClients(data)) });
-});
+function fetchConnectedWifiClients(router_ip, path) {
+  return new Promise((resolve, reject) => {
+    if (typeof router_ip !== 'string') {
+      reject(new Error('no valid router_ip provided'))
+    }
+
+    http.get({
+      host: router_ip,
+      port: 80,
+      path: path || '/wlanAccess.asp',
+      agent: false,
+      }, (res) => {
+      let data = '';
+      res.on('data', chunk => data += chunk);
+      res.on('end', () => {
+        resolve(parseWifiClients(data));
+      });
+    });
+  })
+}
+
+fetchConnectedWifiClients('192.168.1.1')
+  .then(clients => console.log(clients));
