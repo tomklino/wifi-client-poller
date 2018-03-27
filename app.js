719b58cfa17961338b29fba6c8a2a94dc662cdee
Tom Klinovsky <tom@kiwinet.co.il>
2018-03-27 15:47:21 +0300

    moved fetch function to a seperate module

--- app.js
+++ app.js
@@ -1,62 +1,11 @@
 const cheerio = require('cheerio');
-const request = require('request');
 
-const parseWifiClients = require('./project_modules/wifi_client_parser');
+const fetchConnectedWifiClients = require('./project_modules/wifi_client_fetcher');
 
-function loginToRouter({router_ip, username, password, path}) {
-  return new Promise((resolve, reject) => {
-    let post_data = {
-      loginUsername: username,
-      loginPassword: password,
-      rememberMe: "rememberMe"
-    }
-
-    path = path || '/goform/login';
-
-    request.post(
-      'http://' + router_ip + path,
-      { form: post_data },
-      function(err, res, body) {
-        if (res.headers['location'] === '/login.asp') {
-          reject(new Error("login failed"));
-        } else if (res.headers['location'] === '/RgSwInfo.asp') {
-          resolve();
-        } else {
-          reject(new Error("unknown login error"));
-        }
-      }
-    );
-  });
-}
-
-async function fetchConnectedWifiClients({router_ip, username, password, path}) {
-  try {
-    await loginToRouter({router_ip, username, password});
-  } catch (err) {
-    return Promise.reject(err);
-  }
-  return new Promise((resolve, reject) => {
-    if (typeof router_ip !== 'string') {
-      reject(new Error('no valid router_ip provided'))
-    }
-
-    path = path || '/wlanAccess.asp';
-    request.get(
-      'http://' + router_ip + path,
-      function(err, res, body) {
-        const $ = cheerio.load(body);
-        if ($('h1').html().includes("Login")) {
-          reject(new Error("login error"));
-        } else {
-          resolve(parseWifiClients(body));
-        }
-    });
-  })
-}
 
 fetchConnectedWifiClients({
   router_ip: '192.168.1.1',
   username: 'admin',
   password: 'admin'
 }).then(clients => console.log(clients))
-  .catch(err => console.error("login error"))
+  .catch(err => console.error("login error", err))
