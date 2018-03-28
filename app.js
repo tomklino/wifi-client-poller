3e8e40c1953758c31abc7a2063cb7bb8fb5c33ad
Tom Klinovsky <tom@kiwinet.co.il>
2018-03-28 22:50:51 +0300

    writing events to log

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
+const fs = require('fs');
+
 const trackerFactory = require('./project_modules/function_tracker');
 const fetchConnectedWifiClients = require('./project_modules/wifi_client_fetcher');
+
+let log_destination = '/tmp/test.log';
+
+log_fd = fs.openSync(log_destination, 'a+');
+
+function writeToLog(line) {
+  let now = (new Date()).toString()
+  fs.write(log_fd, now + ": " + line + "\n", null, 'utf8', (err) => {
+    if(err) {
+      console.error(now, "error while writing to log");
+    }
+  })
+}
 
 wifiClientsTracker = trackerFactory({
   watchFunction: fetchMyWifiClients
 })
 .setComparator((a,b) => { return a.mac === b.mac;})
 .on('element_join', (joined_elemnts) => {
-  console.log("these guys joined:", joined_elemnts)
+  writeToLog("joined: " + JSON.stringify(joined_elemnts))
 })
 .on('element_left', (left_elements) => {
-  console.log("those guys has left the building", left_elements)
+  writeToLog("left: " + JSON.stringify(left_elements))
 })
 
 setInterval(() => { wifiClientsTracker.call() }, 2000)
