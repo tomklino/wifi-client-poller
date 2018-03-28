8eb39cf73d10226765bb2760ab2019cda1df93dc
Tom Klinovsky <tom@kiwinet.co.il>
2018-03-28 21:33:19 +0300

    completed the PoC phase, showing wifi clients as they join or leave

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
-const cheerio = require('cheerio');
-
+const trackerFactory = require('./project_modules/function_tracker');
 const fetchConnectedWifiClients = require('./project_modules/wifi_client_fetcher');
 
-fetchConnectedWifiClients({
-  router_ip: '192.168.1.1',
-  username: 'admin',
-  password: 'admin'
-}).then(clients => console.log(clients))
-  .catch(err => console.error("login error", err))
+wifiClientsTracker = trackerFactory({
+  watchFunction: fetchMyWifiClients
+})
+
+wifiClientsTracker.on('element_join', (joined_elemnts) => {
+  console.log("these guys joined:", joined_elemnts)
+})
+
+wifiClientsTracker.on('element_left', (left_elements) => {
+  console.log("those guys has left the building", left_elements)
+})
+
+wifiClientsTracker.setComparator((a,b) => {
+  return a.mac === b.mac;
+})
+
+setInterval(() => { wifiClientsTracker.call() }, 2000)
+
+function fetchMyWifiClients() {
+  return fetchConnectedWifiClients({
+    router_ip: '192.168.1.1',
+    username: 'admin',
+    password: 'admin'
+  })
+}
