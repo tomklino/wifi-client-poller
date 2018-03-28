84b7bbc8d33832b8b4386f7336e02936277f9b86
Tom Klinovsky <tom@kiwinet.co.il>
2018-03-28 21:55:55 +0300

    using the new chaining functionality in the app

--- app.js
+++ app.js
@@ -4,17 +4,12 @@ const fetchConnectedWifiClients = require('./project_modules/wifi_client_fetcher
 wifiClientsTracker = trackerFactory({
   watchFunction: fetchMyWifiClients
 })
-
-wifiClientsTracker.on('element_join', (joined_elemnts) => {
+.setComparator((a,b) => { return a.mac === b.mac;})
+.on('element_join', (joined_elemnts) => {
   console.log("these guys joined:", joined_elemnts)
 })
-
-wifiClientsTracker.on('element_left', (left_elements) => {
+.on('element_left', (left_elements) => {
   console.log("those guys has left the building", left_elements)
-})
-
-wifiClientsTracker.setComparator((a,b) => {
-  return a.mac === b.mac;
 })
 
 setInterval(() => { wifiClientsTracker.call() }, 2000)
